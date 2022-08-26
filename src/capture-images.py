4f7cfe028d7d167b6778f58cf5148b562f9f0791
David Hundley <david.k.hundley@gmail.com>
2022-08-26 13:53:45 -0500

    Updating capture-images file

--- src/capture-images.py
+++ src/capture-images.py
@@ -1,8 +1,10 @@
 # Importing the necessary Python libraries
 import cv2
-import uuid
 import os
+import uuid
 import time
+
+
 
 ## FILEPATH INSTANTIATION
 ## ---------------------------------------------------------------------------------------------------------------------
@@ -16,11 +18,10 @@ TESTING_IMAGES_FILEPATH = os.path.join(BASE_IMAGES_FILEPATH, 'test')
 # Collecting the filepaths into an iterable array
 IMAGE_FILEPATHS = [BASE_IMAGES_FILEPATH, TRAINING_IMAGES_FILEPATH, TESTING_IMAGES_FILEPATH]
 
-# Creating the image path if it does not already exist
+# Looping over IMAGE_FILEPATHS to create the directories if they do not already exist
 for image_path in IMAGE_FILEPATHS:
     if not os.path.exists(image_path):
         os.mkdir(image_path)
-
 
 
 ## IMAGE CAPTURE
@@ -28,7 +29,7 @@ for image_path in IMAGE_FILEPATHS:
 # Setting the labels of the image types we will be capturing
 IMAGE_LABELS = ['rock', 'paper', 'scissors']
 
-# Setting number of training / testing images to capture
+# Setting the number of training / testing images to capture
 NUM_TRAIN_IMAGES = 8
 NUM_TEST_IMAGES = 2
 
@@ -44,22 +45,22 @@ for label in IMAGE_LABELS:
     for image_number in range(NUM_TRAIN_IMAGES):
         print(f'Collecting "{label}" training image #{image_number + 1}...')
 
-        # Getting captured frame from the webcam
+        # Getting the captured frame from the webcam
         ret, frame = video_capture.read()
 
         # Creating the appropriate name of the image with a unique identifier
         image_name = os.path.join(TRAINING_IMAGES_FILEPATH, f'{label}.{str(uuid.uuid1())}.jpg')
 
-        # Writing out the image to file
+        # Writing out the image file
         cv2.imwrite(image_name, frame)
 
         # Displaying the captured image
         cv2.imshow('Image Capture', frame)
 
-        # Waiting 2 seconds for next captured image
+        # Waiting 2 seconds for the next captured image
         time.sleep(2)
 
-        # Allowing user to break out of image capture by pressing the "Q" key
+        # Allowing the user to break out of the image capture by pressing the "Q" key
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -69,25 +70,24 @@ for label in IMAGE_LABELS:
     for image_number in range(NUM_TEST_IMAGES):
         print(f'Collecting "{label}" testing image #{image_number + 1}...')
 
-        # Getting captured frame from the webcam
+        # Getting the captured frame from the webcam
         ret, frame = video_capture.read()
 
         # Creating the appropriate name of the image with a unique identifier
         image_name = os.path.join(TESTING_IMAGES_FILEPATH, f'{label}.{str(uuid.uuid1())}.jpg')
 
-        # Writing out the image to file
+        # Writing out the image file
         cv2.imwrite(image_name, frame)
 
         # Displaying the captured image
         cv2.imshow('Image Capture', frame)
 
-        # Waiting 2 seconds for next captured image
+        # Waiting 2 seconds for the next captured image
         time.sleep(2)
 
-        # Allowing user to break out of image capture by pressing the "Q" key
+        # Allowing the user to break out of the image capture by pressing the "Q" key
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
 
 # Ending the image capture script appropriately
 video_capture.release()
